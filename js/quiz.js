ac63cd7686a632e5c50431eca2fba0ea3ba0416f
Pablo <pablo.ledesma.p@gmail.com>
2015-12-02 15:34:47 -0500

    Initial transition

--- js/quiz.js
+++ js/quiz.js
@@ -4,8 +4,16 @@
 
 	app.controller('QuizController',['$scope', '$http', '$sce', function($scope, $http, $sce){
 
+		$scope.score = 0;
+		$scope.activeQuestion = -1;
+		$scope.activeQuestionAnswered = 0;
+		$scope.percentage = 0;
 
+		$http.get('quiz_data.json').then(function(quizData){
+			$scope.myQuestions = quizData.data;
+			$scope.totalQuestions = $scope.myQuestions.length;
 
+		});
 
 	}]);
 
